1cfe5afd00801be928460c5f47c7edd660c5063e
Donna Bactol <bact0002@algonquinlive.com>
2022-09-12 10:52:44 -0400

    Initial commit

---
+++ app/src/main/java/bact0002/cst2335/MainActivity.java
@@ -0,0 +1,14 @@
+package bact0002.cst2335;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
