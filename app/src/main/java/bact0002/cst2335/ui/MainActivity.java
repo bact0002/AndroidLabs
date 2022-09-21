9ee9ee80c283b5b475e6c41217bff20496f9d777
bact0002 <bact0002@algonquinlive.com>
2022-09-21 15:53:51 -0400

    Added Toast feature

--- app/src/main/java/bact0002/cst2335/ui/MainActivity.java
+++ app/src/main/java/bact0002/cst2335/ui/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import bact0002.cst2335.data.MainViewModel;
 import bact0002.cst2335.databinding.ActivityMainBinding;
@@ -31,19 +32,19 @@ public class MainActivity extends AppCompatActivity {
         variableBinding.checkBox.setText("Do you drink coffee?");
         variableBinding.checkBox.setOnCheckedChangeListener(   (button, isChecked) ->   {
             variableBinding.checkBox.setText("You drink coffee." + isChecked);
-           // model.isSelected.postValue(variableBinding.checkBox.getText().equals(isChecked));
+           model.isSelected.postValue(isChecked);
         }     );
 
         variableBinding.switch2.setText("Do you drink coffee?");
         variableBinding.switch2.setOnCheckedChangeListener(   (button, isChecked) ->   {
             variableBinding.switch2.setText("You drink coffee." + isChecked);
-           // model.isSelected.postValue(variableBinding.switch2.getText().equals(isChecked));
+           model.isSelected.postValue(isChecked);
         }     );
 
         variableBinding.radioButton2.setText("Do you drink coffee?");
         variableBinding.radioButton2.setOnCheckedChangeListener(   (button, isChecked) ->   {
             variableBinding.radioButton2.setText("You drink coffee." + isChecked);
-            //model.isSelected.postValue(variableBinding.radioButton2.getText().equals(isChecked));
+            model.isSelected.postValue(isChecked);
         }     );
 
         model.editString.observe(this, s -> variableBinding.textview.setText("Your edit has: " + s));
@@ -52,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
             variableBinding.checkBox.setChecked(selected);
             variableBinding.radioButton2.setChecked(selected);
             variableBinding.switch2.setChecked(selected);
-
+            Toast.makeText(getApplicationContext(),"This value is now: " + selected, Toast.LENGTH_SHORT).show();
         });
 
 
